c5c206a84a9af98b8faa7c200b5820985decac5a
srdjanstefanovic <srdjanstefanovicgit@gmail.com>
2020-06-07 19:07:33 +0200

    Solution Setup

---
+++ MobileShop/MobileShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobileShop.WebUI.Startup))]
+namespace MobileShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
